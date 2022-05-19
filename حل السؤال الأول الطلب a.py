9f82fa1e6ade43102c4592202da2d853fcae24a3
hadeelmohammad563 <105631829+hadeelmohammad563@users.noreply.github.com>
2022-05-19 09:15:39 +0300

    Add files via upload

---
+++ حل السؤال الأول الطلب a.py
@@ -0,0 +1,7 @@
+students=["hadeel","lama","kheder","ghada","wisam"]
+m=input("enter your name: ")
+if (m in students):
+    print(m,"is graduation student")
+else:
+    print(m,"is not gdaruation student")
+
